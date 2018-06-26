6e86d94b5106112232bc2a14fa22044596dacf87
Federico <federico@woowup.com>
2018-06-26 17:03:39 -0300

    modificaciones

--- application/controllers/Wup.php
+++ application/controllers/Wup.php
@@ -20,6 +20,7 @@ class Wup extends CI_Controller {
 	private $processed_folder;
 	private $delimiter;
 	private $orders = [];
+	private $hist = false;
 
 	public function __construct(){
 		parent::__construct();
@@ -50,7 +51,7 @@ class Wup extends CI_Controller {
 		die('fin');
 	}
 
-	public function sales()
+	public function sales($days = 0, $hist = false)
 	{
 
 		$this->load->helper('directory');
@@ -58,12 +59,18 @@ class Wup extends CI_Controller {
 		$this->load->helper('date');
 		$this->load->helper('html');
 
-		$this->file = $this->sales_file.date('Ymd');
+		if ($hist == false) {
+			$this->file = $this->sales_file.date('Ymd', strtotime("-$days days"));
+			$days++;
+			$expected_date = date('Y-m-d 12:00:00', strtotime("-$days days"));
+		} else {
+			$this->file = $this->sales_file . "20180621111859_hist_2015-16-17-18";
+			$this->hist = true;
+		}
 
 		if($this->_downloadFTP()){
 
 			$file_arr = explode(PHP_EOL,read_file($this->pending_folder.'/'.$this->file.'.'.$this->ext));
-
 
 			$total_rows = count($file_arr)-1;
 			$row_index = 0;
@@ -72,6 +79,17 @@ class Wup extends CI_Controller {
 			$i;
 			foreach ($file_arr as $key => $value) {
 				$file_row = str_getcsv($value);
+				if (count($file_row) <= 1) {
+					continue;
+				}
+				$sale_date = array_shift($file_row);
+				$sale_date = date_format(date_create_from_format("d/m/Y", $sale_date), "Y-m-d 12:00:00");
+				if ($hist === false) {
+					if ($expected_date !== $sale_date) {
+						continue;
+					}
+				}
+						
 
 				if(!empty($file_row[1])){
 					if(  !$invoice_number == false
@@ -80,12 +98,7 @@ class Wup extends CI_Controller {
 					   ) {
 						$order = $this->buildOrder($orders);
 						$orders = [];
-
-
 					} elseif($total_rows == $key) {
-
-
-
 						$order = $this->buildOrder($orders);
 						$orders = [];
 					}
@@ -94,28 +107,57 @@ class Wup extends CI_Controller {
 
 						$woowup = new \WoowUp\Client($this->apikey);
 
-						if($woowup->users->exist($order['service_uid'])){
-							if($this->debug){
-								echo '<pre>';
-								print_r($order['invoice_number']);
-								echo '</pre>';
-							}
+						try {
+							if($woowup->users->exist($order['service_uid'])){
+								if($this->debug){
+									echo '<pre>';
+									print_r($order['invoice_number']);
+									echo '</pre>';
+								}
 
-							$woowup->purchases->create($order);
-						} else {
-							if($this->debug){
-								echo '<pre>';
-								echo "Customer no existe : ".$order['service_uid'];
-								echo '</pre>';
+								$order['createtime'] = $sale_date;
+								$woowup->purchases->create($order);
+							} else {
+								if($this->debug){
+									echo '<pre>';
+									echo "Customer no existe : ".$order['service_uid'];
+									echo '</pre>';
+								}
 							}
+						} catch (\Exception $e) {
+							var_dump($e->getMessage());
 						}
-
 					}
 					$order = '';
 					$invoice_number = isset($file_row[1]) ? $file_row[1] : false;
 					$orders[] = $file_row;
 				}
 
+			}
+			if (isset($orders) && isset($sale_date)) {
+				$order = $this->buildOrder($orders);
+				$order['createtime'] = $sale_date;
+				$woowup = new \WoowUp\Client($this->apikey);
+				try {
+					if($woowup->users->exist($order['service_uid'])){
+						if($this->debug){
+							echo '<pre>';
+							print_r($order['invoice_number']);
+							echo '</pre>';
+						}
+
+						$order['createtime'] = $sale_date;
+						$woowup->purchases->create($order);
+					} else {
+						if($this->debug){
+							echo '<pre>';
+							echo "Customer no existe : ".$order['service_uid'];
+							echo '</pre>';
+						}
+					}
+				} catch (\Exception $e) {
+					var_dump($e->getMessage());
+				}
 			}
 			rename($this->pending_folder.'/'.$this->file.'.'.$this->ext, $this->processed_folder.'/'.$this->file.'.'.$this->ext);
 		}
@@ -196,7 +238,7 @@ class Wup extends CI_Controller {
 			$products = [];
 			$woowup = new \WoowUp\Client($this->apikey);
 			foreach ($file_arr as $key => $value) {
-				$file_row = str_getcsv($value,$this->deliminter);
+				$file_row = str_getcsv($value,$this->delimiter);
 
 				$sku = $file_row[0];
 				$item = ['sku' => $file_row[0],
@@ -273,6 +315,7 @@ class Wup extends CI_Controller {
 								 'street' => utf8_decode($file_row[7]),
 								 'country'=> utf8_decode($file_row[8]),
 								 'postcode' => utf8_decode($file_row[9]),
+								 'document' => $file_row[0],
 								];
 
 						if($woowup->users->exist($file_row[0])){
@@ -314,7 +357,7 @@ class Wup extends CI_Controller {
 
 	private function _findFile($item){
 		$item_array = explode("/", $item);
-		if(preg_match('/^'.$this->file.'/', $item_array[3])){
+		if(preg_match('/^'.$this->file.'([0-9]{6}).txt/i', $item_array[3])){
 			return $item;
 		};
 	}
@@ -330,12 +373,16 @@ class Wup extends CI_Controller {
 
 		$this->load->library('ftp');
 		$this->ftp->connect($config);
-		$files = array_filter(array_map(array($this, '_findFile'),$this->ftp->list_files($this->remote_folder)));
-
+		if ($this->hist === false) {		
+			$files = array_filter($this->ftp->list_files($this->remote_folder), array($this, '_findFile'));
+		} else {
+			$files[0] = $this->remote_folder . $this->file . '.' . $this->ext;
+		}
 		if(!empty($files)){
 			$download = true;
-
+			
 			foreach($files as $file){
+				echo "Descargando el archivo $file \n";
 				try {
 					$download = $this->ftp->download($file, $this->pending_folder.'/'.$this->file.'.'.$this->ext, 'ascii');
 
@@ -343,7 +390,7 @@ class Wup extends CI_Controller {
 						throw new Exception("No se pudo descargar el archivo / Archivo no existe.",1);
 					};
 
-					$delete = $this->ftp->delete_file($file);
+					//$delete = $this->ftp->delete_file($file);
 
 				} catch(Exception $error) {
 					echo $error->getMessage();
